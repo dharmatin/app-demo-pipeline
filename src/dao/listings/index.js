42b12d1767c5f89654780ac735f73de6b1ee71c5
dharmatin <dharmatin@gmail.com>
2018-03-27 18:46:31 +0700

    Add change

--- src/dao/listings/index.js
+++ src/dao/listings/index.js
@@ -15,11 +15,17 @@ const search = () => {
         {
           documentid: '6000011',
           channels: 'buy',
-          price: 2500000,
+          price: 25000,
           currency: 'IDR'
         },
         {
           documentid: '6000012',
+          channels: 'buy',
+          price: 32000,
+          currency: 'IDR'
+        },
+        {
+          documentid: '6000013',
           channels: 'buy',
           price: 3200000,
           currency: 'IDR'
